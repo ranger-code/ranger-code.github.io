b0ef191d1d82465eae5dcf5fdd888fb1b820a23b
ranger-code <varadpvinod09@gmail.com>
2024-02-21 16:04:50 +0530

    Rework - Stage 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
-const navlink = document.querySelectorAll(".navlink");
+const navlink = document.querySelectorAll("li");
 const underline = document.querySelectorAll(".underline");
-const coords = { x: 0, y: 0 };
-const trailer = document.getElementById("trailer");
-const magic = document.querySelectorAll(".magic");
 
 window.addEventListener("mousemove", function(e){
     coords.x = e.clientX - trailer.offsetWidth/2;
@@ -15,13 +12,9 @@ window.addEventListener("mousemove", function(e){
 
 for (let i = 0; i <= navlink.length; i++) {
     navlink[i].addEventListener("mouseover", function () {
-        trailer.style.backgroundColor = "rgba(255, 255, 255, 0.10)";
         underline[i].style.width="100%";
-        // magic[i].classList.toggle("magiked")
     });
     navlink[i].addEventListener("mouseout", function () {
-        trailer.style.backgroundColor = "rgba(255, 255, 255, 1)";
         underline[i].style.width="0%";
-        // magic[i].classList.toggle("magic");
     });
 }
